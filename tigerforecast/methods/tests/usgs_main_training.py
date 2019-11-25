0c9815dcb76f0ea6a3f1160ba79f717bd7dec557
Cyril Zhang <cyril.zhang@princeton.edu>
2019-11-25 21:17:40 +0000

    fix data occlusion

--- tigerforecast/methods/tests/usgs_main_training.py
+++ tigerforecast/methods/tests/usgs_main_training.py
@@ -7,6 +7,9 @@ import pickle
 from tigerforecast.utils import generate_key
 from usgs_data_loader import *
 
+from tigerforecast.utils.optimizers import *
+from tigerforecast.utils.optimizers.losses import *
+
 TRAINING_STEPS = 30000
 BATCH_SIZE = 1024
 SEQUENCE_LENGTH = 61
@@ -14,13 +17,13 @@ HIDDEN_DIM = 100
 EMBEDDING_DIM = 10
 DATA_PATH = '~/data/usgs_{}.csv'
 
-loss = lambda pred, true: np.mean(np.sum((pred - true)**2, axis=(1,2)))
+optim = Adam(loss=batched_mse, learning_rate=0.1)
 
 usgs_train = USGSDataLoader(DATA_PATH.format('train'))
 usgs_val = USGSDataLoader(DATA_PATH.format('val_mini'), site_idx=usgs_train.site_idx, normalize_source=usgs_train)
 
 method_LSTM = tigerforecast.method("FloodLSTM")
-method_LSTM.initialize(n=9, m=1, l = 61, h = HIDDEN_DIM, e_dim = EMBEDDING_DIM, num_sites = len(usgs_train.site_keys), optimizer = None)
+method_LSTM.initialize(n=8, m=1, l = 61, h = HIDDEN_DIM, e_dim = EMBEDDING_DIM, num_sites = len(usgs_train.site_keys), optimizer=optim)
 
 results_LSTM = []
 pred_LSTM = []
@@ -40,7 +43,8 @@ for i, (data, targets) in enumerate( usgs_train.random_batches(batch_size=BATCH_
 	#print(y_pred_LSTM[0,:,0])
 	#print(targets[0,:])
 	targets_exp = np.expand_dims(targets, axis=-1)
-	results_LSTM.append(loss(jax.device_put(targets_exp), y_pred_LSTM))
+	loss = float(batched_mse(jax.device_put(targets_exp), y_pred_LSTM))
+	results_LSTM.append(loss)
 	method_LSTM.update(targets_exp)
 
 	if i%100 == 0:
