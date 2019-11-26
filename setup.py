6fdf912381c86dd7766263317743d64668023d79
John Hallman <johnolof@gmail.com>
2019-11-26 13:48:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,21 @@ import setuptools
 # 0.xx = alpha, 1.xx = beta, >2.xx = full release
 version = '0.1'
 
+Cython>=0.22
+pystan>=2.14
+numpy>=1.10.0
+pandas>=0.23.4
+matplotlib>=2.0.0
+LunarCalendar>=0.0.9
+convertdate>=2.1.2
+holidays>=0.9.5
+setuptools-git>=1.2
+
+extras = {
+  'prophet': ['prophet', 'Cython', 'pystan', 'LunarCalendar', 'convertdate', 'holidays', 'setuptools-git'],
+}
+extras['all'] = [item for group in extras.values() for item in group]
+
 setuptools.setup(
     name='tigerforecast',
     url='https://github.com/MinRegret/TigerForecast',
@@ -12,6 +27,7 @@ setuptools.setup(
     author_email='johnolof@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=open('requirements.txt').read().split('\n'),
+    extras_require=extras,
     version=version,
     license='Apache License 2.0',
     description='Princeton time-series framework',
